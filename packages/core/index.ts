b77b741144e4c5fd3ff2626a7db380c1deebaf10
Peter Brownell <greenman@the-organization.com>
2026-03-06 17:46:35 +0000

    Fix core package runtime barrel export

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,8 +1,1 @@
-// Services
-export { MeetingService } from './src/services/meeting-service';
-
-// Interfaces
-export { IMeetingRepository } from './src/interfaces/i-meeting-repository';
-
-// Re-export types from schema for convenience
-export type { Meeting, CreateMeeting, UpdateMeeting } from '@repo/schema';
+export * from './src/index';
